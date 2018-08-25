b936e2bb2359908c8b17c9c8d70a682613b335ad
Cosmo Myzrail Gorynych aka Comigo <admin@nersta.ru>
2018-08-25 17:11:40 +1200

    :bento: Add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/img');
-    eleventyConfig.addPassthroughCopy('src/fonts');
+    eleventyConfig.addPassthroughCopy('src/browserconfig.xml');
+    eleventyConfig.addPassthroughCopy('src/manifest.json');
+    eleventyConfig.addPassthroughCopy('src/favicon.ico');
     
     return {
         dir: {
