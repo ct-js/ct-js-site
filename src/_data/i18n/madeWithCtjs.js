2442f3ee84d02ec1a95b23c96223641ace5ba037
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-10-29 09:34:27 +1200

    :zap: DRY made with ct.js page

---
+++ src/_data/i18n/madeWithCtjs.js
@@ -0,0 +1,32 @@
+module.exports = {
+    en: {
+        pageTitle: 'Games made with ct.js — Ct.js',
+        pageDescription: 'Games created with ct.js, a free 2D game editor based on web technologies.',
+        madeWithCths: 'Made with ct.js',
+        submitProject: 'Submit Your Project',
+        joinGamedev: 'Join the world of game development',
+        gamedevIsFree: 'It\'s free!',
+        download: 'Download',
+        play: 'Play',
+    },
+    br: {
+        pageTitle: 'Jogos feitos com ct.js — Ct.js',
+        pageDescription: 'Jogos criados com o ct.js, um editor de jogos 2D grátis baseado em tecnologias de web.',
+        madeWithCths: 'Feito com ct.js',
+        submitProject: 'Envie seu projeto',
+        joinGamedev: 'Entre no mundo de desenvolvimentos de jogos',
+        gamedevIsFree: 'é grátis!',
+        download: 'Baixar',
+        play: 'Jogar',
+    },
+    ru: {
+        pageTitle: 'Игры, сделанные на коте — ct.js',
+        pageDescription: 'Игры, написанные на ct.js, бесплатном редакторе 2D игр на веб-технологиях.',
+        madeWithCths: 'Сделано в ct.js',
+        submitProject: 'Добавить свой проект',
+        joinGamedev: 'Присоединись к миру геймдева',
+        gamedevIsFree: 'Это бесплатно!',
+        download: 'Скачать',
+        play: 'Играть',
+    }
+};
