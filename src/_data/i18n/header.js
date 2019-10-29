7b83a1be4b3068d5bcf23eca4b009c56b5bf54c6
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-10-29 16:51:21 +1200

    :sparkles: A proper language selector

--- src/_data/i18n/header.js
+++ src/_data/i18n/header.js
@@ -4,20 +4,23 @@ module.exports = {
         docs: 'Docs',
         forum: 'Forum',
         download: 'Download',
-        gotoHomepage: 'Go to the homepage'
+        gotoHomepage: 'Go to the homepage',
+        language: 'En'
     },
     br: {
         changelog: 'Notas de Atualizações',
         docs: 'Documentação',
         forum: 'Forum',
         download: 'Baixar',
-        gotoHomepage: 'Ir para o inicio'
+        gotoHomepage: 'Ir para o inicio',
+        language: 'Pt-Br'
     },
     ru: {
         changelog: 'Лог изменений',
         docs: 'Документация',
         forum: 'Форум',
         download: 'Скачать',
-        gotoHomepage: 'На главную'
+        gotoHomepage: 'На главную',
+        language: 'Ru'
     }
 };
