963f006334a9d50599c07d4fcee6d1036eedb3bb
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-10-29 13:06:20 +1200

    :zap: DRY "Join Gamedev" section

---
+++ src/_data/i18n/joinGamedev.js
@@ -0,0 +1,17 @@
+module.exports = {
+    en: {
+        joinGamedev: 'Join the world of game development',
+        gamedevIsFree: 'It\'s free!',
+        download: 'Download',
+    },
+    br: {
+        joinGamedev: 'Entre no mundo de desenvolvimentos de jogos',
+        gamedevIsFree: 'é grátis!',
+        download: 'Baixar',
+    },
+    ru: {
+        joinGamedev: 'Присоединись к миру геймдева',
+        gamedevIsFree: 'Это бесплатно!',
+        download: 'Скачать',
+    }
+};
