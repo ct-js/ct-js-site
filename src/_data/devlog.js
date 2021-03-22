499aa7eb55ae5a95e2163ad6e09fb4383e64d0ca
Cosmo Myzrail Gorynych <admin@nersta.ru>
2021-03-22 15:20:42 +1200

    :zap: Update eleventy, add request caching

--- src/_data/devlog.js
+++ src/_data/devlog.js
@@ -1,24 +1,22 @@
-const axios  = require('axios'),
-      toJSON = require('xml2js').parseString;
+const toJSON = require('xml2js').parseString;
+const Cache = require('@11ty/eleventy-cache-assets');
 
 const url = 'https://comigo.itch.io/ct/devlog.rss';
 
 module.exports = () => {
-    return new Promise((resolve, reject) => {
-        axios.get(url)
-        .then((response) => {
-            // turn the feed XML into JSON
-            toJSON(response.data, function (err, result) {
-                const posts = result.rss.channel[0].item;
-                posts.length = Math.min(posts.length, 5);
-                for (const post of posts) {
-                    post.image = String(post.description).split('<p>')[0];
-                }
-                resolve({'url': url, 'posts': posts});
-            });
-        })
-        .catch((error) => {
-            reject(error);
+    return Cache(url, {
+        duration: '1d', // save for 1 day
+        type: 'xml'
+    })
+    .then(data => new Promise(resolve => {
+        // turn the feed XML into JSON
+        toJSON(data, function (err, result) {
+            const posts = result.rss.channel[0].item;
+            posts.length = Math.min(posts.length, 5);
+            for (const post of posts) {
+                post.image = String(post.description).split('<p>')[0];
+            }
+            resolve({'url': url, 'posts': posts});
         });
-    });
+    }));
 };
