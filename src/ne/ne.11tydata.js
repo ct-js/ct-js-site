431451c854fc22f7008794fc3ad8ae570cfd7503
GambleBranch <122032368+GambleBranch@users.noreply.github.com>
2023-01-08 10:52:43 +0100

    📝Add Dutch translation

---
+++ src/ne/ne.11tydata.js
@@ -0,0 +1,12 @@
+module.exports = {
+    site: {
+        locale: 'ne',
+        links: {
+            download: '/ne/download',
+            changelog: '/ne/changelog',
+            madeWithCtjs: '/ne/madeWithCtjs',
+            contact: '/ne/contact',
+            docs: 'https://docs.ctjs.rocks/' // No docs in Dutch yet unfortunately
+        }
+    }
+};
