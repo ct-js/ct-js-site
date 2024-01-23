2c25a8ca928103ca0b80e1ed44345b587147f1fc
wesam <108880473+wesamdev@users.noreply.github.com>
2024-01-23 17:11:18 +0400

    Update src/_data/i18n/homepage.js
    Co-authored-by: Aymen <aymennasrii2002@gmail.com>

--- src/_data/i18n/homepage.js
+++ src/_data/i18n/homepage.js
@@ -118,7 +118,7 @@ const voc = {
 
         devlog: {
             latest: 'مستجدات البرمجة',
-            readMore: 'اقرا المزيد…',
+            readMore: '...اقرأ المزيد',
             morePosts: 'المزيد من المنشورات',
         },
 
